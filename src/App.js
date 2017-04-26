ab373fed763d6d4fac2c37ecae08584ba87e3473
cbudtz <chrbudtz@gmail.com>
2017-04-26 13:17:08 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -10,13 +10,41 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            color: "hotpink"
+            color: "hotpink",
+            navbar:[
+                {type:"NavItem",id:"F17/02324/0",text:"Agenda"},
+                {type:"NavItem",id:"F17/02324/1",  text:"Kursus oversigt"},
+                {type:"NavDropDown",id:"F17/02324/2",text:"Kurser",items:[
+                    {type:"MenuItem",id:"0",text:"test"}
+                    ]}
+            ]
         }
     }
-    test(){
+    test = (e) => {
         this.setState({
             color:"Yellow"
         })
+    }
+    getNavContent = () =>{
+        var content = this.state.navbar.map((nav, no) =>{
+            if(nav.type=="NavItem") {
+               return <NavItem key={nav.id} eventKey={nav.id}>{nav.text}</ NavItem>
+            } else {
+                console.log(nav.items);
+                var items = nav.items.map((item,no) =>{
+                   return <MenuItem key={item.id}>{item.text}</MenuItem>
+                })
+                console.log(items)
+                return <NavDropdown id={nav.id} title={nav.text}>
+                    {items}
+                </NavDropdown>
+            }
+        })
+        return content;
+    }
+    handleNavSelect = (e) =>{
+        this.setState({page:e})
+        console.log(e);
     }
 
   render() {
@@ -25,12 +53,21 @@ class App extends Component {
       <div className="App">
           <div className="NavbarContainer">
               <Navbar>
+
                   <Navbar.Header>
-                      Test
+                      <Navbar.Brand >
+                          test
+                      </Navbar.Brand>
+                      <Navbar.Toggle/>
                   </Navbar.Header>
+                  <Navbar.Collapse>
+                  <Nav onSelect={this.handleNavSelect}>
+                      {this.getNavContent()}
+                  </Nav>
+                  </Navbar.Collapse>
               </Navbar>
           </div>
-        <div className="App-header">
+        <div className="container">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome {this.state.color} {this.props.name} to React with hot reload</h2>
         </div>
---
+++ src/LoginPage.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Christian on 07-04-2017.
+ */
+import React, {Component} from 'react';
+import {Row,Col} from 'react-bootstrap';
+
+class LoginPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+
+    }
+}
+export default LoginPage;
